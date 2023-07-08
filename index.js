84ba0e3af4ec5ecd5994c3f137961f9d2d26928b
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-07-08 14:35:58 +0300

    assignment completed

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ fetch('http://localhost:3000/menuItems')
       const newItem = document.createElement('div');
       newItem.classList.add('item');
 
-      const itemImg = document.createElement('div');
-      itemImg.classList.add('item-img');
-      const img = document.createElement('img');
-      img.src = item.image;
-      itemImg.appendChild(img);
-      newItem.appendChild(itemImg);
+      // const itemImg = document.createElement('div');
+      // itemImg.classList.add('item-img');
+      // const img = document.createElement('img');
+      // img.src = item.image;
+      // itemImg.appendChild(img);
+      // newItem.appendChild(itemImg);
 
       const itemDescription = document.createElement('div');
       itemDescription.classList.add('item-description');
